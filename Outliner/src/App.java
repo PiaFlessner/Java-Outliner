1f15716ca41b228e20105706d08895ff5c65bf7d
pia.flessner <pia.flessner@stud.hs-emden-leer.de>
2023-02-06 17:31:00 +0100

    start of Header object

--- Outliner/src/App.java
+++ Outliner/src/App.java
@@ -1,5 +1,17 @@
+import javax.swing.*;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Runnable guiCreator = new Runnable(){
+            public void run(){
+                JFrame fenster = new JFrame("hallo Welt mit Swing");
+                fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+                fenster.setSize(300,200);
+                fenster.setVisible(true);
+            }
+        };
+
+        SwingUtilities.invokeLater(guiCreator);
     }
+
 }
