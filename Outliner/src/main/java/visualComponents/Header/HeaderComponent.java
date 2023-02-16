6a75aa09a78f51abd6249f27c0552b38e8d53d32
pia.flessner <pia.flessner@stud.hs-emden-leer.de>
2023-02-16 15:03:05 +0100

    FocusColor for Header and Icon focus remove

--- Outliner/src/main/java/visualComponents/Header/HeaderComponent.java
+++ Outliner/src/main/java/visualComponents/Header/HeaderComponent.java
@@ -1,28 +1,29 @@
 package main.java.visualComponents.Header;
 
 import javax.swing.JLabel;
-import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-
 import main.java.backendData.Header;
-
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.BorderLayout;
 import java.awt.Color;
-import javax.swing.BorderFactory;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import javax.swing.KeyStroke;
+import javax.swing.event.MouseInputAdapter;
+
 public class HeaderComponent extends JPanel{
 
     JPanel headerTitle;
-    JLabel arrowIcon;
+    Icon icon;
 
     JLabel displayedNumber;
     JTextField displayedNumberEdit;
     
-    JLabel displayedHeaderTitle;
+    Title displayedHeaderTitle;
     JTextField displayedHeaderTitleEdit;
 
     JPanel headerContent;
@@ -32,6 +33,7 @@ public class HeaderComponent extends JPanel{
     final Color EDIT_COLOR = new Color(242,242,242);
     final int HEADERCONTAINER_FOLDED_HEIGHT = 40;
     final int HEADERCONTAINER_UNFOLDED_HEIGHT = 200;
+    final Color FOCUS_COLOR = new Color(214,220,229);
 
     boolean isOpen;
     Header connectedHeader;
@@ -46,6 +48,9 @@ public class HeaderComponent extends JPanel{
         
         setUpHeaderTitle();
         setUpContent();
+        this.setFocusable(true);
+        addFocusingFunction();
+        setUpOpenHeaderFunction();
    
     }
 
@@ -59,13 +64,13 @@ public class HeaderComponent extends JPanel{
         headerTitle.setLayout(titleContainerFlowLayout);
 
         //Icon adding
-        Icon icon = new Icon(this);
+        icon = new Icon();
         headerTitle.add(icon);
 
         setUpDisplayedNumber();
 
         //Title adding
-        Title displayedHeaderTitle = new Title(this);
+        displayedHeaderTitle = new Title(this);
         headerTitle.add(displayedHeaderTitle);
         this.add(headerTitle, BorderLayout.NORTH);
     }
@@ -91,5 +96,59 @@ public class HeaderComponent extends JPanel{
 
         this.add(headerContent,BorderLayout.CENTER);
 
+    }
+
+    private void openClose(){
+        if(this.isOpen){
+            //Close Header
+            icon.setArrowClose();
+            this.isOpen = false;
+            this.setMaximumSize(new Dimension(2147483647,this.HEADERCONTAINER_FOLDED_HEIGHT));
+            this.textArea.setVisible(false);
+            this.textArea.textArea.setFocusable(false);
+        }
+        else{
+            //Open Header
+            icon.setArrowOpen();
+            this.isOpen = true;
+            this.setMaximumSize(new Dimension(2147483647, this.HEADERCONTAINER_UNFOLDED_HEIGHT));
+            this.textArea.setVisible(true);
+            this.textArea.textArea.setFocusable(true);
+        }
+    }
+
+    private void setUpOpenHeaderFunction(){
+        //KeyStroke Function
+        this.getInputMap().put(KeyStroke.getKeyStroke("SPACE"), "OpenCloseHeader");
+        this.getActionMap().put("OpenCloseHeader", new AbstractAction(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                openClose();
+                System.out.println("HallO");
+            }
+        });
+
+        //Button Click Function
+        this.addMouseListener(new MouseInputAdapter() {
+            @Override
+            public void mouseClicked(java.awt.event.MouseEvent e) {
+                openClose();
+                System.out.println("HallO");
+            }           
+        });
+    }
+
+    private void addFocusingFunction(){
+        this.addFocusListener(new FocusAdapter(){
+            @Override
+            public void focusGained(FocusEvent e) {
+                headerTitle.setBackground(FOCUS_COLOR);
+            } 
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                headerTitle.setBackground(backgroundColor);
+            }
+        });
     }
 }
--- Outliner/src/main/java/visualComponents/Header/Icon.java
+++ Outliner/src/main/java/visualComponents/Header/Icon.java
@@ -1,95 +1,33 @@
 package main.java.visualComponents.Header;
 
 import javax.swing.JLabel;
-import javax.swing.KeyStroke;
-
-import java.awt.event.ActionEvent;
-import java.awt.event.FocusAdapter;
-import java.awt.event.FocusEvent;
 import java.awt.Dimension;
-import java.awt.Color;
 import javax.swing.SwingConstants;
-import javax.swing.event.MouseInputAdapter;
-
-
 import java.awt.Cursor;
-import javax.swing.AbstractAction;
-import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
-
-
 import java.io.File;
 public class Icon extends JLabel{
     
-
-    HeaderComponent parent;
     final String ARROW_ICON = "src"+File.separator+"main"+File.separator+"resources"+File.separator+"arrow.png";
     final String ARROW_ICON_DOWN = "src"+File.separator+"main"+File.separator+"resources"+File.separator+"arrowDown.png";
+    final ImageIcon arrowOpen = new ImageIcon(ARROW_ICON_DOWN);
+    final ImageIcon arrow = new ImageIcon(ARROW_ICON);
 
 
-    public Icon(HeaderComponent parent){
+    public Icon(){
         this.setHorizontalAlignment(SwingConstants.CENTER);
         this.setIcon(new ImageIcon(ARROW_ICON));
         this.setToolTipText("");
         this.setCursor(new Cursor(Cursor.HAND_CURSOR));
         this.setHorizontalTextPosition(SwingConstants.CENTER);
         this.setPreferredSize(new Dimension(20, 20));
-        this.setFocusable(true);
-
-        this.parent = parent;
-        addFocusingFunction();
-        setUpOpenHeaderFunction();
     }
 
-    private void openClose(){
-        if(parent.isOpen){
-            //Close Header
-            this.setIcon(new ImageIcon(ARROW_ICON));
-            parent.isOpen = false;
-            parent.setMaximumSize(new Dimension(2147483647,parent.HEADERCONTAINER_FOLDED_HEIGHT));
-            parent.textArea.setVisible(false);
-            parent.textArea.textArea.setFocusable(false);
-        }
-        else{
-            //Open Header
-            this.setIcon(new ImageIcon(ARROW_ICON_DOWN));
-            parent.isOpen = true;
-            parent.setMaximumSize(new Dimension(2147483647, parent.HEADERCONTAINER_UNFOLDED_HEIGHT));
-            parent.textArea.setVisible(true);
-            parent.textArea.textArea.setFocusable(true);
-        }
+    public void setArrowOpen(){
+        this.setIcon(this.arrowOpen);
     }
 
-    private void addFocusingFunction(){
-        this.addFocusListener(new FocusAdapter(){
-            @Override
-            public void focusGained(FocusEvent e) {
-                setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY));
-            } 
-
-            @Override
-            public void focusLost(FocusEvent e) {
-                setBorder(null);
-            }
-        });
-    }
-
-    private void setUpOpenHeaderFunction(){
-        //KeyStroke Function
-        this.getInputMap().put(KeyStroke.getKeyStroke("SPACE"), "OpenCloseHeader");
-        this.getActionMap().put("OpenCloseHeader", new AbstractAction(){
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                openClose();
-            }
-        });
-
-        //Button Click Function
-        this.addMouseListener(new MouseInputAdapter() {
-            @Override
-            public void mouseClicked(java.awt.event.MouseEvent e) {
-                openClose();
-            }           
-        });
+    public void setArrowClose(){
+        this.setIcon(this.arrow);
     }
 }
