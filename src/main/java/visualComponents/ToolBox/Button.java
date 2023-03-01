2d6a0e993a0fad3c1c9aa51df926e1669e1a40ec
pia flessner <pia.flessner@stud.hs-emden-leer.de>
2023-03-01 02:01:57 +0100

    double focus in button removed

--- src/main/java/visualComponents/ToolBox/Button.java
+++ src/main/java/visualComponents/ToolBox/Button.java
@@ -1,14 +1,12 @@
 package main.java.visualComponents.ToolBox;
 
 import java.awt.Color;
-
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class Button extends JButton{
-
     public Button(Color backgroundColor, Color foregroundColor, JPanel parent){
+        this.setFocusPainted(false);
         this.setBackground(backgroundColor);
         this.setForeground(foregroundColor);
         parent.add(this);
