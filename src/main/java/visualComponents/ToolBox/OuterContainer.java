d726acc1aa327547ea63fde10cabfe10ff39d1fd
pia.flessner <pia.flessner@stud.hs-emden-leer.de>
2023-03-12 23:09:49 +0100

    change variable to final

--- src/main/java/visualComponents/ToolBox/OuterContainer.java
+++ src/main/java/visualComponents/ToolBox/OuterContainer.java
@@ -3,11 +3,12 @@ package main.java.visualComponents.ToolBox;
 import javax.swing.JPanel;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.FlowLayout;
 import java.awt.Color;
 
 public class OuterContainer extends JPanel {
     private final Dimension CONTAINER_DIMENSION = new Dimension(62, 38);
-    private GridLayout CONTAINER_GRIDLAYOUT = new GridLayout(1, 0);
+    private final GridLayout CONTAINER_GRIDLAYOUT = new GridLayout(1, 0);
 
     public OuterContainer(Color backgroundColor){
         this.setBackground(backgroundColor);
